949a5beedac874dd71fedb838f263c46265fd0a2
Zahnen Garner <zahnen@gmail.com>
2021-01-26 16:55:00 -0800

    major styling overhaul
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- Guestly/Controllers/HomeController.cs
+++ Guestly/Controllers/HomeController.cs
@@ -3,21 +3,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Linq;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using Guestly.Models;
 
 namespace Guestly.Controllers
 {
     public class HomeController : Controller
     {
-      // private readonly GuestlyContext _db;
+      private readonly GuestlyContext _db;
 
-      // public HomeController(GuestlyContext db)
-      // {
-      //   _db = db;
-      // }
+      public HomeController(GuestlyContext db)
+      {
+        _db = db;
+      }
 
       public ActionResult Index()
       {
+      List<DataPoint> dataPoints = new List<DataPoint>{
+				new DataPoint(10, 22, label:"King"),
+				new DataPoint(20, 36, label:"Queen"),
+				new DataPoint(30, 42, label:"Full"),
+				new DataPoint(40, 51, label:"Suite"),
+				new DataPoint(50, 40, label:"Baby"),
+      };
+
+			ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
         return View();
       }
 
